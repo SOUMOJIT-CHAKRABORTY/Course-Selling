79b7cd0e81f8944755f8f70e649476c49dec5267
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-07-11 18:45:02 +0530

    basic landing page created

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,11 +1,30 @@
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
+
 const Nav = () => {
   const navigate = useNavigate();
   return (
     <div className="flex pb-10  justify-between items-center">
-      <div className="text-4xl font-semibold">Coursehere</div>
-      <div className="flex space-x-4 ">
+      <div className="text-4xl font-semibold text-blue-500">Coursehere</div>
+      <div className="flex space-x-6 ">
+        <IconButton aria-label="cart">
+          <StyledBadge badgeContent={0} color="secondary">
+            <ShoppingCartIcon />
+          </StyledBadge>
+        </IconButton>
         <Button onClick={() => navigate("/login")} variant="outlined">
           Login
         </Button>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,64 @@
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Hero from "../assets/Hero.jpg";
+import Google from "../assets/google.png";
+import Airbnb from "../assets/airbnb.png";
+import Cm from "../assets/CreativeMarket.png";
+import Amazon from "../assets/amazon.png";
+import Shopify from "../assets/shopify.png";
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
     <div className="mt-12">
-      <div className="flex justify-around space-x-20 items-center">
-        <div>
-          Text
-          <div>
+      <div className="flex justify-between items-center space-x-10 xl:space-x-20">
+        <div className="lg:text-5xl md:text-4xl text-2xl font-semibold flex flex-col">
+          <div className="mb-6 whitespace-nowrap">
+            Gain expertise in your chosen field,
+          </div>
+          <div className="mb-6 whitespace-nowrap">
+            explore new horizons, and accelerate
+          </div>
+          <div className="mb-8 text-blue-500 whitespace-nowrap">
+            your career growth
+          </div>
+          <div className="text-base font-medium text-gray-500 max-w-[70%]">
+            {`With CourseHere, you have the key to success right at your
+            fingertips. Embark on a transformative learning experience designed
+            to inspire, empower, and guide you towards greatness. Join a global
+            community of learners, connect with industry experts, and ignite
+            your passion for continuous learning. Whether you're a student,
+            professional, or lifelong learner, CourseHere offers a diverse range
+            of courses tailored to your unique goals and aspirations.`}
+          </div>
+          <div className="mt-10 flex  items-center  space-x-8 text-xl">
             <Button onClick={() => navigate("/signup")}>Get started</Button>
-            <Button onClick={() => navigate("/login")} variant="contained">
+            <Button
+              onClick={() => navigate("/login")}
+              variant="contained"
+              sx={{
+                color: "#fff",
+              }}
+            >
               Learning
             </Button>
           </div>
         </div>
-        <div>
-          <img src={Hero} alt="image here" className="h-[540px] w-[500px] " />
+        <div className="min-w-[20%] max-w-[40%]">
+          <img
+            src={Hero}
+            alt="image here"
+            className="h-[540px] w-full object-contain "
+          />
         </div>
+      </div>
+      <div className="flex mt-20 justify-center md:space-x-10 lg:space-x-14 xl:space-x-20">
+        <img src={Google} className="bg-white h-16" alt="" />
+        <img src={Airbnb} className="bg-white h-16" alt="" />
+        <img src={Shopify} className="bg-white h-16" alt="" />
+        <img src={Cm} className="bg-white h-16" alt="" />
+        <img src={Amazon} className="bg-white h-16" alt="" />
       </div>
     </div>
   );
