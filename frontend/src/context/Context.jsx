6472bf91e6bc826d97a4dbaf30c4c49fe173b9e4
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2023-08-01 00:00:16 +0530

    new context added

--- frontend/src/context/Context.jsx
+++ frontend/src/context/Context.jsx
@@ -1,28 +1,9 @@
 import PropTypes from "prop-types";
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
 
 export const CourseDetails = createContext();
 
 export const CourseData = (props) => {
-  const [courses, setCourses] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const token = localStorage.getItem("token");
-      const headers = new Headers();
-      headers.append("Authorization", `Bearer ${token}`);
-      const response = await fetch("http://localhost:3000/users/courses", {
-        method: "GET",
-        headers: headers,
-      });
-
-      const data = await response.json();
-
-      setCourses(data.courses);
-    };
-    fetchData();
-  }, []);
-
   return (
     <CourseDetails.Provider value={[courses, setCourses]}>
       {props.children}
